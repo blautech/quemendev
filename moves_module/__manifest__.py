4a4a830c926336fa09f4d8dc1b542448a084b939
FreduxR <fredyreyesorantes@gmail.com>
2020-07-29 15:18:53 -0600

    Moves module

---
+++ moves_module/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Inventory Modifications',
+    'version': '1.0',
+    'description': 'Inventory Modifications',
+    'author': 'Blautech',
+    'company': 'Blautech',
+    'website': "https://blautech.us/",
+    'data': ['views/template.xml']
+
+}
---
+++ moves_module/models/moves_config.py
@@ -0,0 +1,5 @@
+import logging
+import psycopg2
+from odoo import models, fields, api, tools
+
+_logger = logging.getLogger(__name__)
