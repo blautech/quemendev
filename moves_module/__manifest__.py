984a1f19122632f21b5ecda0d6f094b64a6ccaef
FreduxR <fredyreyesorantes@gmail.com>
2020-07-29 18:33:17 -0600

    Add dependencies module

--- moves_module/__manifest__.py
+++ moves_module/__manifest__.py
@@ -5,7 +5,7 @@
     'author': 'Blautech',
     'company': 'Blautech',
     'website': "https://blautech.us/",
-    'depends': ['base', 'stock'],
+    'depends': ['base', 'stock_move_line'],
     'data': ['views/template.xml']
 
 }
